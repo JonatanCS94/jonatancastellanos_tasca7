24e9b1e82fb4bfe686d3c16df8d9a233eee8c2d8
JonatanCS94 <jonatancastellanossanchez94@gmail.com>
2017-12-19 13:20:34 +0100

    Actualización 19/12/2017

--- fechasjavascript.js
+++ fechasjavascript.js
@@ -1,5 +1,5 @@
 function calcularsiglo(a){
-   
+   b = calcularanyo(b);
     if (b>=1700 && b<=1799){
         return 5;
     }
@@ -22,70 +22,97 @@ function calcularsiglo(a){
 }
 
 function calcularanyo(b){
-//var b = Math.trunc(ParseInt(b.toString().substr(-2))/4);
-
-var n = ParseInt(b.toString().substr(-2));
+var b = calcularanyo(b);
+var n = parseInt(b.toString().substr(-2));
 var m = n/4;
-var o = Math.trunc(m+n);
+var o = Math.trunc(m);
+var p = n+o;
 
-return o;
+return p;
 }
 
 function calcularmes(c){
+var c = calcularmes(c);
 
-
-    if (b == "enero"){
+    if (c == "enero"){
         return 6;
     }
-    else if (b == "febrero"){
+    else if (c == "febrero"){
         return 2;
     }
-    else if (b == "marzo"){
+    else if (c == "marzo"){
         return 2;
     }
-    else if (b == "abril"){
+    else if (c == "abril"){
         return 5;
     }
-    else if (b == "mayo"){
+    else if (c == "mayo"){
         return 0;
     }
-    else if (b == "junio"){
+    else if (c == "junio"){
         return 3;
     }
-    else if (b == "julio"){
+    else if (c == "julio"){
         return 5;
     }
-    else if (b == "agosto"){
+    else if (c == "agosto"){
         return 1;
     }
-    else if (b == "septiembre"){
+    else if (c == "septiembre"){
         return 4;
     }
-    else if (b == "octubre"){
+    else if (c == "octubre"){
         return 6;
     }
-    else if (b == "noviembre"){
+    else if (c == "noviembre"){
         return 2;
     }
-    else if (b == "diciembre"){
+    else if (c == "diciembre"){
         return 4;
     }
 }
 function añobisiesto(d){
-    if (b.toString().substr(1, 1) == "2" || b.toString().substr(1, 1) == "4" && d == enero || d == febrero){
+    var b = calcularanyo(b);
+    if (b.toString().substr(-1) == "2" || b.toString().substr(-1) == "4" && d == enero || d == febrero){
        return -1;
     }
-    else if (b.toString().substr(1, 1) == "2" || b.toString().substr(1, 1) == "4" && d != enero || d != febrero){
+    else if (b.toString().substr(-1) == "2" || b.toString().substr(-1) == "4" && d != enero || d != febrero){
         return 0;
     }
 }
 
 function dia(e){
+    var e = dia(e);
     return e;
     }
 
 function diadelasemana(r){
+    var a = calcularsiglo(a);
+    var b = calcularanyo(b);
+    var c = calcularmes(c);
+    var d = añobisiesto(d);
+    var e = dia(e);
     var w = a+b+c+d+e;
     var z = w - 7;
-    return z;
+    if (z == 0){
+        return "domingo";
+    }
+    else if (z == 1){
+        return "lunes";
+    }
+    else if (z == 2){
+        return "martes";
+    }
+    else if (z == 3){
+        return "miércoles";
+    }
+    else if (z == 4){
+        return "jueves";
+    }
+    else if (z == 5){
+        return "viernes";
+    }
+    else if (z == 6){
+        return "sábado";
+    }
 }
